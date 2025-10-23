5e072147f2ec490929e3ca975afa5dde31193469
LynxSaiko <redteamsectorid@gmail.com>
2025-10-23 10:46:17 +0700

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -5,7 +5,7 @@
 set -eu  # Keluar segera jika perintah gagal atau variabel tidak disetel
 
 # --- KONFIGURASI PENGGUNA ---
-LIVE_NAME="LFS_Live"
+LIVE_NAME="leakos"
 LFS_SOURCE_ROOT="/"
 # Partisi yang akan dicari oleh script 'init' saat booting.
 LIVE_BUILD_DIR="$(find /mnt/liveiso/ -type d -name "${LIVE_NAME}-build*" | head -n1)
